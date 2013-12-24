1a77b0d19412894f55f764ff3621d8fbed369747
Adriano Tadao <drianotadao@gmail.com>
2013-12-24 01:21:55 -0200

    Arrumando path e url dos uploads

--- app/models/stamp.rb
+++ app/models/stamp.rb
@@ -1,7 +1,7 @@
 class Stamp < Asset
   has_attached_file :data,
-    path: 'rails_root/public/system/:attachment/:fingerprint/:id/:filename',
-    url: 'system/:attachment/:fingerprint/:id/:filename'
+    path: ':rails_root/public/system/stamps/:id_partition/:fingerprint/:basename_:style.:extension',
+    url: 'system/stamps/:id_partition/:fingerprint/:basename_:style.:extension'
 
   validates_attachment_presence :data
   validates_attachment_content_type :data, content_type: 'image/png'
